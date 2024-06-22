693a46a6698a15be055e997ab80e88ece1161d6d
Danil <abdushaev04@list.ru>
2024-06-22 17:10:01 +0300

    доделал ежедневные задачи, но почему-то съехали кнопки в родительском аккаунте не понимаю почему

--- app/src/main/java/com/example/family_task_tracker/Entering_daily_tasks.java
+++ app/src/main/java/com/example/family_task_tracker/Entering_daily_tasks.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -26,22 +25,22 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
-public class Entering_bonus_task extends AppCompatActivity {
+public class Entering_daily_tasks extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private DatabaseReference mRef;
-    private TextView textView;
-    private EditText editTextTime;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setContentView(R.layout.activity_entering_bonus_task);
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_daily_tasks);
+
         DisplayMetrics bt = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(bt);
         int width = bt.widthPixels;
         int height = bt.heightPixels;
         getWindow().setLayout((int) (width * .8), (int) (height * .7));
-        textView = findViewById(R.id.textView10);
-        editTextTime = findViewById(R.id.editTextTime);
+        //textView = findViewById(R.id.textView10);
+        //editTextTime = findViewById(R.id.editTextTime);
 
         WindowManager.LayoutParams params = getWindow().getAttributes();
         params.gravity = Gravity.CENTER;
@@ -50,22 +49,24 @@ public class Entering_bonus_task extends AppCompatActivity {
         getWindow().setAttributes(params);
 
 
-        Button upload_task = findViewById(R.id.upload_task);
-        EditText Name_task = findViewById(R.id.Name_task);
-        EditText Task_control = findViewById(R.id.Task_conditions);
-
-        super.onCreate(savedInstanceState);
         mAuth = FirebaseAuth.getInstance();
         mRef = FirebaseDatabase.getInstance().getReference();
         FirebaseUser User = mAuth.getInstance().getCurrentUser();
         DatabaseReference UserRef = mRef.child("users").child(User.getUid());
-        upload_task.setOnClickListener(new View.OnClickListener() {
+
+        Button upload_daily_tasks_BT = findViewById(R.id.upload_daily_tasks_BT);
+        EditText Name_task = findViewById(R.id.daily_tasks_name_Et);
+        EditText Task_control = findViewById(R.id.daily_tasks_condition_Et);
+
+
+
+        upload_daily_tasks_BT.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (Name_task.getText().toString().isEmpty() || Task_control.getText().toString().isEmpty()) {
                     Toast.makeText(getApplicationContext(), "Fields cannot be empty", Toast.LENGTH_SHORT).show();
                 } else {
-                    long seconds = Long.parseLong(editTextTime.getEditableText().toString());
+                    /*long seconds = Long.parseLong(editTextTime.getEditableText().toString());
                     CountDownTimer timer = new CountDownTimer(seconds * 1000, 1000) {
                         @Override
                         public void onTick(long millisUntilFinished) {
@@ -77,13 +78,13 @@ public class Entering_bonus_task extends AppCompatActivity {
                             textView.setText("0");
                         }
                     };
-                    timer.start();
+                    timer.start();*/
                     //String key_name = mRef.child("users").child(User.getUid()).child("tasks").push().getKey();
                     //mRef.child("users").child(User.getUid()).child("tasks").setValue(Name_task.getText().toString());
                     Map<String,Object> task_map = new HashMap<>();
                     String str_Guid = String.valueOf(java.util.UUID.randomUUID());
-                    task_map.put("tasks/"+"bonus_tasks/"+str_Guid+"/name_task", Name_task.getText().toString());
-                    task_map.put("tasks/"+"bonus_tasks/"+str_Guid+"/condition", Task_control.getText().toString());
+                    task_map.put("tasks/"+"daily_tasks/"+str_Guid+"/name_task", Name_task.getText().toString());
+                    task_map.put("tasks/"+"daily_tasks/"+str_Guid+"/condition", Task_control.getText().toString());
                     UserRef.updateChildren(task_map);
                     //Map<String,Object> task_condition = new HashMap<>();
 
@@ -91,14 +92,13 @@ public class Entering_bonus_task extends AppCompatActivity {
                     finish();
                 }
             }
+
         });
-        EdgeToEdge.enable(this);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }
-
-
--- app/src/main/java/com/example/family_task_tracker/Parent_account.java
+++ app/src/main/java/com/example/family_task_tracker/Parent_account.java
@@ -14,10 +14,17 @@ public class Parent_account extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parent_account);
         ImageButton raz_zadachi_btn = findViewById(R.id.raz_zadachi_btn);
+        ImageButton daily_tasks_btn = findViewById(R.id.daily_tasks_btn);
         raz_zadachi_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Parent_account.this,Entering_bonus_task.class));
+            }
+        });
+        daily_tasks_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Parent_account.this, Entering_daily_tasks.class));
             }
         });
     }
