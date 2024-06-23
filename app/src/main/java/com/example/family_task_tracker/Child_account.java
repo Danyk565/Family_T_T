29cacc4aea5fff7b808c55e654aeebbfee895fe6
Danil <abdushaev04@list.ru>
2024-06-23 20:07:11 +0300

    Свёл аккаунты

--- app/src/main/java/com/example/family_task_tracker/Child_account.java
+++ app/src/main/java/com/example/family_task_tracker/Child_account.java
@@ -3,18 +3,31 @@ package com.example.family_task_tracker;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import java.util.ArrayList;
 
 
 public class Child_account extends AppCompatActivity {
     ArrayList<Tasks> task = new ArrayList<Tasks>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child_account);
+        ImageButton link_parent = findViewById(R.id.link_parent_btn);
+
+        link_parent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            startActivity(new Intent(Child_account.this, Link_parent.class));
+            }
+        });
         // начальная инициализация списка
-        setInitialData();
+        /*setInitialData();
         RecyclerView recyclerView = findViewById(R.id.rvView);
         // создаем адаптер
         TasksAdapter adapter = new TasksAdapter(this, task);
@@ -28,5 +41,6 @@ public class Child_account extends AppCompatActivity {
         task.add(new Tasks ("Колумбия", "Богота", 400));
         task.add(new Tasks ("Уругвай", "Монтевидео", 200));
         task.add(new Tasks ("Чили", "Сантьяго", 600));
+    }*/
     }
 }
---
+++ app/src/main/java/com/example/family_task_tracker/Link_parent.java
@@ -0,0 +1,93 @@
+package com.example.family_task_tracker;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Link_parent extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private DatabaseReference mRef;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_link_parent);
+
+        Button Save_parent = findViewById(R.id.save_parent_btn);
+        EditText parentRef = findViewById(R.id.parent_email_et);
+
+        mAuth = FirebaseAuth.getInstance();
+        mRef = FirebaseDatabase.getInstance().getReference();
+        FirebaseUser User = mAuth.getInstance().getCurrentUser();
+        DatabaseReference UserRef = mRef.child("users").child(User.getUid());
+        DatabaseReference Users = mRef.child("users");
+
+
+        Save_parent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String parentEmail = parentRef.getText().toString();
+                Query query = Users.orderByChild("email").equalTo(parentEmail);
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists()) {
+                            String parentUID="";
+                            for (DataSnapshot userSnapshot : snapshot.getChildren()) {
+                                 parentUID = userSnapshot.getKey().toString();
+                            }
+                            Map<String,Object> parent_map = new HashMap<>();
+                            parent_map.put("parent",parentUID);
+                            UserRef.updateChildren(parent_map);
+                            finish();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+       /*mAuth.fetchSignInMethodsForEmail(parentEmail).
+                addOnCompleteListener(task -> {
+                    if(task.isSuccessful()){
+                        FirebaseUser parent = mAuth.getCurrentUser();
+                        if(parent!=null){
+                            String parentUID = parent.getUid();
+                            Map<String,Object> parent_map = new HashMap<>();
+                            parent_map.put("parent",parentUID);
+                            UserRef.updateChildren(parent_map);
+                            finish();
+                        }
+                    }
+                });*/
+            }
+        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+}
